7c9dea27bb9cc4156ee1916a1cd89dab7f68388e
Hachim Khaldi <hachim.khaldi@jaanga.ca>
2023-12-14 14:41:43 +0100

    Initial

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const express = require('express');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });;
+//const io = socketIO(server);
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  // Listen for messages from the client
+  socket.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    // Send a response back to the client
+    io.emit('message', `Server received: ${message}`);
+  });
+  // Listen for the socket to disconnect
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
